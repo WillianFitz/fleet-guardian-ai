dabe1c850c00aa8e37ef33eba69ea49b53bb869b
WillianFitz <willian.fitzbr@gmail.com>
2026-02-23 13:36:49 -0300

    Update 014_add_chave_origem_valor_total.sql

--- d1-migrations/014_add_chave_origem_valor_total.sql
+++ d1-migrations/014_add_chave_origem_valor_total.sql
@@ -1,10 +1,6 @@
 -- Migration: adiciona colunas chave_origem e valor_total na tabela ctes
-PRAGMA foreign_keys=off;
-BEGIN TRANSACTION;
-
+-- Observação: Cloudflare D1 (Wrangler) rejeita BEGIN/COMMIT em execuções remotas.
+-- Aqui usamos apenas os ALTER TABLE independentes.
 ALTER TABLE ctes ADD COLUMN chave_origem TEXT;
 ALTER TABLE ctes ADD COLUMN valor_total REAL DEFAULT 0;
-
-COMMIT;
-PRAGMA foreign_keys=on;
 
