908a3f47d024e6251cd90447b9b80c5652ea2f83
WillianFitz <willian.fitzbr@gmail.com>
2026-02-24 13:58:10 -0300

    Update 014_add_updated_at_drivers.sql

--- d1-migrations/014_add_updated_at_drivers.sql
+++ d1-migrations/014_add_updated_at_drivers.sql
@@ -1,10 +1,6 @@
-BEGIN TRANSACTION;
-
 -- Add updated_at column to drivers (no constant default allowed in ALTER TABLE)
 ALTER TABLE drivers ADD COLUMN updated_at TEXT;
 
 -- Populate existing rows with current timestamp where null
 UPDATE drivers SET updated_at = datetime('now') WHERE updated_at IS NULL;
-
-COMMIT;
 
